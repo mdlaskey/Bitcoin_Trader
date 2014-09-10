195cb76e8714193c80247410944cee410c93d99c
Michael Laskey <michaelluskey@michaels-MacBook-Pro-6.local>
2014-09-10 11:08:40 -0700

    added API safety

--- main.py
+++ main.py
@@ -9,6 +9,7 @@ import visuals
 import numpy
 import cPickle as pickle
 import time
+import urllib2 
 
 PRIVATE_KEY = '9bd15515d2c34f582eddd4975d03bb73f50761d7ef628a2d0f6f66551dcd642bd7dc30747400185f'
 PUBLIC_KEY = '2f2ef8959a68c1912fe51b6cbee0fc973798854b'
@@ -28,20 +29,24 @@ if __name__ == '__main__':
 	States = []
 	Trade_Data = []
 	i = 0
-	while( i < 2):
+	while( i < 1000):
 
-		m_dict = C.getMarkets()
-		m_id,price = M.get_market(m_dict)
-		m_trades = C.marketTrades(m_id)
+		try:
+			m_dict = C.getMarkets()
+			m_id,price = M.get_market(m_dict)
+			m_trades = C.marketTrades(m_id)
 
-		trade_data = T.get_data(m_trades)
-		price_EMA = T.EMA(trade_data[:,1])
+			trade_data = T.get_data(m_trades)
+			price_EMA = T.EMA(trade_data[:,1])
 
-		wallet = W.get_wallet()
+			wallet = W.get_wallet()
 
-		state = Q.get_state(price_EMA,price,wallet,0)
-		States.append(state)
-		Trade_Data.append(trade_data)
+			state = Q.get_state(price_EMA,price,wallet,0)
+			States.append(state)
+			Trade_Data.append(trade_data)
+		except:
+			print "API error"
+			
 		print i
 		#V.plot_trades(trade_data)
 		i = i+1
